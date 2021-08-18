2aff29d0d13a46d8f9c5ac1cfca501284a203c21
inc0der <inc0der.code@gmail.com>
2021-08-18 19:01:11 -0300

    Add basic project automation (#3)
    * Add macro that copies the plugins details json from plugin in game
    
    * Add macro for copying details to compiler
    
    * Add details json copied from macro
    
    * Add output macro to auto set output files via a defined argument in hxml
    
    * Remove redundant trace
    
    * Update path for readability
    
    * Rename from tasks.Macros to Macros

---
+++ tasks/Macros.hx
@@ -0,0 +1,30 @@
+#if macro
+import sys.io.File;
+import sys.FileSystem;
+import haxe.macro.Context;
+import haxe.macro.Compiler;
+#end
+
+class Macros {
+ #if macro
+ static var pluginName = Context.definedValue('plugin-name');
+ static var pluginDir = '${Sys.getCwd()}/game/resources/app/Content/Datas/Scripts/Plugins/${Macros.pluginName}';
+
+ public static function copyDetails() {
+  if (pluginName != null) {
+   if (FileSystem.exists(pluginDir)) {
+    File.copy('${pluginDir}/details.json', '${Sys.getCwd()}/details.json');
+   }
+  }
+ }
+
+ public static function setOutput() {
+  var isDist = Context.definedValue('dist');
+  if (isDist != null) {
+   Compiler.setOutput('${Sys.getCwd()}/dist/code.js');
+  } else {
+   Compiler.setOutput('${Macros.pluginDir}/code.js');
+  }
+ }
+ #end
+}
