0e05593d12652c12d098c89147beb8b8b5e1787f
inc0der <inc0der.code@gmail.com>
2021-08-18 15:57:10 -0300

    Add style rules (#1)
    * Add style and format rules
    
    * Update files to use new style rules

--- src/GameReload.hx
+++ src/GameReload.hx
@@ -4,25 +4,25 @@ import electron.renderer.Remote;
 import js.Browser;
 
 class GameReload {
-  public static function setup() {
-		var reloadShortcut = Plugins.getParameter('DevTools', 'Reload Shortcut');
-		if (reloadShortcut != '') {
-      GameReload.register();
-			Browser.window.addEventListener('focus', GameReload.register);
-			Browser.window.addEventListener('blur', GameReload.unregister);
-			Browser.window.addEventListener('beforeunload', GameReload.unregister);
-    }
+ public static function setup() {
+  var reloadShortcut = Plugins.getParameter('DevTools', 'Reload Shortcut');
+  if (reloadShortcut != '') {
+   GameReload.register();
+   Browser.window.addEventListener('focus', GameReload.register);
+   Browser.window.addEventListener('blur', GameReload.unregister);
+   Browser.window.addEventListener('beforeunload', GameReload.unregister);
   }
+ }
 
-  static function register() {
-		GlobalShortcut.register('F5', GameReload.reload);
-  }
-  
-  static function unregister() {
-		GlobalShortcut.unregister('F5');
-  }
+ static function register() {
+  GlobalShortcut.register('F5', GameReload.reload);
+ }
 
-  static function reload() {
-    Remote.getCurrentWindow().reload();
-  }
+ static function unregister() {
+  GlobalShortcut.unregister('F5');
+ }
+
+ static function reload() {
+  Remote.getCurrentWindow().reload();
+ }
 }
